76ca1008d7f2ecbc41090246bff87681864bde07
Niko142 <baya29.03.1977@gmail.com>
2025-07-05 15:30:20 +0400

    refactor: improve project build
    - correction errors in Docker-build
    - add nginx-configuration for correct work
    - db-connection now autimatically choose correct host

--- server/db/database.js
+++ server/db/database.js
@@ -3,12 +3,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// Автоматическое определение host в зависимости от вариант сборки проекта
+const host = process.env.DOCKER_ENV === 'true' ? 'db' : 'localhost';
+
 const db = new Pool({
-  host: process.env.HOST,
+  // host: process.env.HOST,
+  host: host,
   user: process.env.USER_NAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT,
+  ssl: false,
 });
 
 // Подключение к БД
